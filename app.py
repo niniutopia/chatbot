45d917eb2ce5d1c6b500b40458f2afde7e28b929
niniutopia <nicchan707@gmail.com>
2026-05-13 16:03:41 +0200

    ersdcv

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
 import streamlit as st
 
-with st.chat_message("user"):
-    st.write("Hello 👋")
+
+st.title("Bottino carino")
 
 with st.chat_message("assistant"):
     st.write("Hello human")
-
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
-
-#############
-
-st.title("Echo Bot")
-
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
 ###########
 if prompt := st.chat_input("What is up?"):
