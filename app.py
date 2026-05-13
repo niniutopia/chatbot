fba2f73ec52c5be508726603343459d19257c491
niniutopia <nicchan707@gmail.com>
2026-05-13 16:02:12 +0200

    ersdcv

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import numpy as np
 
 with st.chat_message("user"):
     st.write("Hello 👋")
 
 with st.chat_message("assistant"):
     st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
 
 prompt = st.chat_input("Say something")
 if prompt:
@@ -24,3 +22,11 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+###########
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
